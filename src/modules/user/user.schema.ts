d8c07d22770da2baac3e133483d1bf14cf7079ef
Eduardo Zirbell <eduardo.zirbell@lincros.com>
2024-08-07 07:54:44 -0300

    Enviando alterações para máquina local

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,6 +1,7 @@
-import {z} from 'zod'
+import { z } from 'zod'
+import { buildJsonSchemas } from 'fastify-zod';
 
-const createUserSchema = z.object({
+const userCore = {
     email: z.string({
         required_error: 'É necessário informar o email.',
         invalid_type_error: 'Email deve ser uma string.'
@@ -9,10 +10,19 @@ const createUserSchema = z.object({
         required_error: 'É necessário informar o nome.',
         invalid_type_error: 'Nome deve ser uma string.'
     }),
+}
+
+const createUserSchema = z.object({
+    ...userCore,
+    id: z.number(),
     password: z.string({
         required_error: 'É necessário informar a senha.',
         invalid_type_error: 'Senha deve ser uma string.'
     }),
 });
+
+const createUserResponseSchema = z.object({
+    ...userCore,
+})
 
 export type CreateUserInput = z.infer<typeof createUserSchema>
--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,9 +1,13 @@
 import cryptography from 'crypto'
 
-export function hashPassword(){
+export function hashPassword(password: string) {
+    const salt = cryptography.randomBytes(16).toString('hex');
+    const hash = cryptography.pbkdf2Sync(password, salt, 1000, 64, 'sha512');
 
+    return { hash, salt };
 }
 
-export function verifyPassword(){
-
+export function verifyPassword(unverifiedPassword: string, salt: string, hash: string) {
+    const unverifiedHash = cryptography.pbkdf2Sync(unverifiedPassword, salt, 1000, 64, 'sha512').toString('hex');
+    return unverifiedHash === hash;
 }
